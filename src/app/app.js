bd08d94e2d139e5b1e442991c8c55edf85f7f080
Adriano Lemos <adriano.lemos@tst.jus.br>
2017-10-30 08:43:14 -0200

    SOME RESPONSIVENESS

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './app.css';
-import Image from '../assets/lemos.jpg';
+import Image from '../assets/lemos.png';
 import Pearls from './pearls/pearls';
 
 export const App = (props) => {
